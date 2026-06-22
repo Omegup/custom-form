5d0061f34e284e4df296a7f7860454675b2eb31e
x <x>
2026-06-22 13:17:42 +0100

    form

---
+++ src/form/form-react.t.ts
@@ -0,0 +1,77 @@
+import type React from "react";
+import type { ContextDom, ExtraDom, ParamsDom, TypedFormItem, VariantsDom, ViewExtraKeys } from "./form.t";
+
+export type GetChild = { getChild: (suffix: string, index: number) => React.ReactNode }
+export type WithChildren<Extra, ExtraView = Extra> = {
+  view: Extra & { children: React.ReactElement[] }
+  children: ExtraView
+}
+export type WithGetChild<Extra> = {
+  view: Extra & GetChild
+  children: Extra
+}
+
+export type Viewer<
+  in Params extends ParamsDom<K>,
+  in Variants extends VariantsDom<K>,
+  in K extends string,
+  in Extra extends ExtraDom,
+  in Context extends ContextDom,
+> = (props: {
+  props: ViewerProps<Params, Variants, K, Extra, Context>
+}) => React.ReactNode
+
+export type ViewerProps<
+  out Params extends ParamsDom<K>,
+  out Variants extends VariantsDom<K>,
+  out K extends string,
+  out Extra extends ExtraDom,
+  out Context extends ContextDom,
+> = {
+  formItem: TypedFormItem<Params, K>
+  ctx: Context
+  extra: Extra
+  variant: Variants[K]
+}
+
+export type Viewers<
+  in TypeNames extends string,
+  in Params extends ParamsDom<TypeNames>,
+  in Variants extends VariantsDom<TypeNames>,
+  in Extra extends Record<'view' | 'children', ExtraDom>,
+  in Context extends ContextDom,
+  out ChildInstanceId,
+> = {
+  [K in TypeNames]: {
+    viewer: Viewer<Params, Variants, K, Extra['view'], Context>
+    repeatChildren?: (
+      formItem: TypedFormItem<Params, K>,
+      extra: Extra['children'],
+    ) => ChildInstanceId[]
+  }
+}
+
+export type FormItemProps<
+  Params extends ParamsDom<K>,
+  Variants extends VariantsDom<K>,
+  K extends string,
+  Extra extends Record<ViewExtraKeys, ExtraDom>,
+  Context extends ContextDom,
+> = {
+  renderCard: (
+    view: React.ReactNode,
+    viewProps: ViewerProps<Params, Variants, K, Extra['children'], Context>,
+  ) => React.ReactNode
+  viewProps: ViewerProps<Params, Variants, K, Extra['view'], Context>
+}
+
+export type RenderCard<
+  TypeNames extends string,
+  Params extends ParamsDom<TypeNames>,
+  Variants extends VariantsDom<TypeNames>,
+  Extra extends ExtraDom,
+  Context extends ContextDom,
+> = <K extends TypeNames>(
+  view: React.ReactNode,
+  viewProps: ViewerProps<Params, Variants, K, Extra, Context>,
+) => React.ReactNode
